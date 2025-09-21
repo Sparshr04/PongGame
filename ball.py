a21ae8285a700228cc96e5baaf5c12ffc4273bd3
Sparsh Rannaware <rannawaresparsh@gmail.com>
2025-09-21 14:51:41 +0530

    Final Game Ready

--- ball.py
+++ ball.py
@@ -11,6 +11,7 @@ class Ball(Turtle):
         self.color('white')
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
 
     def move(self):
@@ -23,6 +24,12 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_pos(self):
+        self.goto(0,0)
+        self.bounce_x()
+        self.move_speed = 0.1
 
     
     
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from score import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+score = Scoreboard()
+
 
 
 screen.listen()
@@ -24,7 +27,7 @@ screen.onkey(fun=r_paddle.go_down, key='Down')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
     screen.update()
 
@@ -35,6 +38,16 @@ while game_is_on:
     # detect collusion with the padddle
     if ball.distance(r_paddle) < 50 and ball.xcor() >  320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    #detect when Right Paddle misses
+    if ball.xcor() > 360:
+        ball.reset_pos()
+        score.l_point()
+
+    #detect when left paddle misses
+    if ball.xcor() < -360:
+        ball.reset_pos()
+        score.r_point()
 
 
 screen.exitonclick()
